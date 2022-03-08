5e65aa8af6c5b136a16339ad280c8d081f1acd40
Karim SEBBAH <Karim.Sebbah@etu.univ-paris1.fr>
2022-03-08 12:58:56 +0100

    refactor(entity): update Pack entity

--- src/Entity/Pack.php
+++ src/Entity/Pack.php
@@ -16,6 +16,26 @@ class Pack
     #[ORM\Column(type: 'string', length: 255)]
     private $nomPack;
 
+    #[ORM\Column(type: 'integer')]
+    private $nbJour;
+
+    #[ORM\Column(type: 'float')]
+    private $Prix;
+
+    #[ORM\OneToOne(targetEntity: Destination::class, cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private $destination;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $descritpion;
+
+    #[ORM\Column(type: 'integer')]
+    private $nbPersonneMax;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $description;
+
+
     public function getId(): ?int
     {
         return $this->id;
@@ -29,6 +49,78 @@ class Pack
     public function setNomPack(string $nomPack): self
     {
         $this->nomPack = $nomPack;
+
+        return $this;
+    }
+
+    public function getNbJour(): ?int
+    {
+        return $this->nbJour;
+    }
+
+    public function setNbJour(int $nbJour): self
+    {
+        $this->nbJour = $nbJour;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->Prix;
+    }
+
+    public function setPrix(float $Prix): self
+    {
+        $this->Prix = $Prix;
+
+        return $this;
+    }
+
+    public function getDestination(): ?Destination
+    {
+        return $this->destination;
+    }
+
+    public function setDestination(Destination $destination): self
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    public function getDescritpion(): ?string
+    {
+        return $this->descritpion;
+    }
+
+    public function setDescritpion(?string $descritpion): self
+    {
+        $this->descritpion = $descritpion;
+
+        return $this;
+    }
+
+    public function getNbPersonneMax(): ?int
+    {
+        return $this->nbPersonneMax;
+    }
+
+    public function setNbPersonneMax(int $nbPersonneMax): self
+    {
+        $this->nbPersonneMax = $nbPersonneMax;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
