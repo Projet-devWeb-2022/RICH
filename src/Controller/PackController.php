493f1357a909d6b8fbd181d5820ac0f42483bfc3
Karim SEBBAH <Karim.Sebbah@etu.univ-paris1.fr>
2022-04-05 00:25:00 +0200

    refactor: modified path for destination page in homepage

--- src/Controller/PackController.php
+++ src/Controller/PackController.php
@@ -8,7 +8,6 @@ use App\Entity\Pack;
 use App\Form\PackType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
-use Knp\Component\Pager\PaginatorInterface;
 use App\Repository\PackRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,9 +15,24 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 
 class PackController extends AbstractController
 {
+    /**
+     * @Route("/pack")
+     */
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        //Récuperer l'ensemble des destinations de notre base et l'envoyer en parametre a notre vue
+        $pack = $doctrine->getRepository(Pack::class);
+        $listePack = $pack->findAll();
+
+        return $this->render('pack/packPage.html.twig', [
+            'controller_name' => 'PackController', 'listePack'=>$listePack
+        ]);
+    }
+
     /**
      * @Route("/admin/pack/all", name="allPacks")
      */
