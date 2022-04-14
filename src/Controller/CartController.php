aaaef2ef826c0edf5900ac53ff9178aa88866bf1
Nicolas Raharinosy <mamilala.raharinosy@etu.univ-paris1.fr>
2022-04-14 22:34:44 +0200

    feat: payment api + redirect when success/cancel

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,10 +5,13 @@ namespace App\Controller;
 use App\Repository\DestinationRepository;
 use App\Repository\PackRepository;
 use App\Service\Cart\CartService;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class CartController extends AbstractController
 {
@@ -49,6 +52,29 @@ class CartController extends AbstractController
 
     }
 
+    #[Route('/checkout', name: 'checkout')]
+    public function checkout($stripeSK): Response
+    {
+        Stripe::setApiKey($stripeSK);
+        //$total = $this->getTotal();
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'eur',
+                    'product_data' => [
+                        'name' => 'Votre commande RICH',
+                    ],
+                    'unit_amount' => 200,
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+        return $this->redirect($session->url, 303);
+    }
 
     public function add($id, CartService $cartService){
         $cartService->add($id);
--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PaymentController extends AbstractController
 {
-    #[Route('/payment', name: 'payment')]
-    public function index(): Response
+
+    #[Route('/payment/success', name: 'success_url')]
+    public function successUrl(MailerInterface $mailer): Response
     {
-        return $this->render('payment/index.html.twig', [
-            'controller_name' => 'PaymentController',
-        ]);
+        $date = new \DateTime();
+        $email = (new Email())
+            ->from('webrich235@gmail.com')
+            ->to($this->getUser()->getEmail())
+
+            ->subject('Votre commande du '. $date->format("D-d-M-Y"))
+            ->text('Votre commande a bien été confirmée ! Vous retrouverez vos informations de paiement dans l\'onglet commandes de votre espace personnel');
+
+
+        $mailer->send($email);
+        return $this->render('payment/success.html.twig', []);
     }
+
+
+    #[Route('/payment/cancel', name: 'cancel_url')]
+    public function cancelUrl(): Response
+    {
+        return $this->render('payment/cancel.html.twig', []);
+    }
+
+
 }
